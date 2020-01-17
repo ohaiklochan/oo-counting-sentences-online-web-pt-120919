56e356cb829581178cd7d730bac993f642647816
Chloe Foronda <caforonda103@yahoo.com>
2020-01-17 00:40:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.delete_if{|sentence| sentence.empty?}
+    array.length
   end
 end
